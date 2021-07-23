e4db1ba43ca372937aa484156dc58ff949f9aee5
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-23 12:04:34 +0530

    flask

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # ---> Importing html file to flask
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    # ---> Importing html file to flask
+    return render_template("about.html")
+
+app.run(debug=True)
